c4bafd028de6b428fea7f824a093e2d59ec19de3
huangyuan <wshy1121@qq.com>
2016-03-04 15:04:04 +0800

    [add]添加readline模块 by huangyuan

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@
 #include "net_server.h"
 #include "net_client.h"
 #include "SimpleIni.h"
-
+#include "CmdLineInter.h"
 
 
 int main(int argc, char* argv[])
 {
 
 	CNetServer::instance()->startServer();
-	
+	sleep(1);
 	CSimpleIniA ini;  
 	ini.SetUnicode();  
 	ini.LoadFile("Config.ini");  
 	const char * netCliIp = ini.GetValue("NetConfig", "NetCliIp", "default");  
 	int netCliPort = (int)ini.GetLongValue("NetConfig", "NetCliPort");
 
-	trace_start(netCliIp, netCliPort, "C:\\Users\\huangyuan1\\Desktop\\Debug1.cpp");
+	trace_start(netCliIp, netCliPort, "/root/nfs/Log/LogManagerDebug.cpp");
 	
 	bool bRet = CNetClient::instance()->connect((char *)"127.0.0.1");
 	if (bRet)
@@ -40,9 +40,21 @@ int main(int argc, char* argv[])
 	CNetClient::instance()->createAccess(access, accessLen);
 	CNetClient::instance()->getAccessRep(access, accessLen, accessRep);
 	CNetClient::instance()->verifyAccess(access, accessLen, accessRep);
+
+    char *line, *s;
+    initialize_readline();
 	while (1)
 	{
-		sleep(1);
+        line = readline("\r> ");
+        if (!line || !strcmp(line, "disconnect1234"))
+    	{
+			break;
+		}
+        s = strstrip(line);
+        if (*s) {
+            add_history(s);
+            execute_line(s);
+        }
 	}
 	return 0;
 }
